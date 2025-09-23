3161bed13e2072eb628fdb52a34ead97b2c00443
LeoneJM-CS <188081003+LeoneJM-CS@users.noreply.github.com>
2025-09-23 10:15:22 -0500

    Add files via upload

--- C#FinalMain.cs
+++ C#FinalMain.cs
@@ -1,12 +1,93 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace FinalProj
 {
-    public class FinalProj()
+    class FinalProj()
     {
-        static void Main(string[] args)
+        public class User()
         {
+            private string password;
+            private string userName;
+            public SortedDictionary<string, string> users;
+            public string Password
+            {
+                get { return password; }
+                set
+                {
+                    if (value == "")
+                    {
+                        Console.WriteLine("Please Try Again\n");
+                    }
+                    else
+                    {
+                        password = value;
+                    }
+                }
+            }
+            public string UserName
+            {
+                get { return userName; }
+                set
+                {
+                    if (value == "")
+                    {
+                        Console.WriteLine("Please Try Again\n");
+                    }
+                    else
+                    {
+                        userName = value;
+                    }
+                }
+            }
+            public SortedDictionary<string, string> Users
+            {
+                get { return users; }
+                set
+                {
+                    users.Add(userName, password);
+                }
 
+            }
+            internal class MainProgram
+            {
+                static void Main(string[] args)
+                {
+                    User user = new User();
+                    user.users = new SortedDictionary<string, string>();
+                    while (true)
+                    {
+                        Console.WriteLine("Enter Your option: \n1. Login\n2. View users\n3. Exit Program");
+                        int? userIn = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("");
+                        if (userIn == 1)
+                        {
+                            Console.WriteLine("Enter Your username: ");
+                            string? username = Console.ReadLine();
+                            Console.WriteLine("Enter Your password: ");
+                            string? pass = Console.ReadLine();
+                            user.Password = pass;
+                            user.UserName = username;
+                            user.users.Add(username, pass);
+                        }
+
+                        else if (userIn == 2)
+                        {
+                            foreach (KeyValuePair<string, string> kv in user.users)
+                            {
+                                Console.WriteLine("Username: " + kv.Key + " " + "Password: " + kv.Value);
+                            }
+                        }
+
+                        else if (userIn == 3)
+                        {
+                            Console.WriteLine("Exiting Program");
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
